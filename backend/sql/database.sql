ebb6243c7ca8cfeaa1a4808f1481232a34ae78d2
Eamon <the.oman.2@gmail.com>
2020-05-18 18:29:34 +1000

    New table for students attending lessons, shortened some variable lengths

--- backend/sql/database.sql
+++ backend/sql/database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Students (
-  Name varchar(255) NOT NULL,
+  Name varchar(50) NOT NULL,
   Age int,
   Mobile varchar(10) NOT NULL,
 
@@ -7,15 +7,15 @@ CREATE TABLE IF NOT EXISTS Students (
 );
 
 CREATE TABLE IF NOT EXISTS Staff (
-  Name varchar(255) NOT NULL,
+  Name varchar(50) NOT NULL,
   Mobile varchar(10) NOT NULL,
 
   ADD CONSTRAINT PK_Staff_Mobile PRIMARY KEY (Mobile)
 );
 
 CREATE TABLE IF NOT EXISTS StaffSubjects (
-  Name varchar(255) NOT NULL,
-  Subject varchar(50) NOT NULL,
+  Name varchar(50) NOT NULL,
+  Subject varchar(10) NOT NULL,
 
   ADD CONSTRAINT PK_Staff_Subjects PRIMARY KEY (Name, Subject),
 
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS StaffSubjects (
 CREATE TABLE IF NOT EXISTS Lessons (
   Day varchar(10) NOT NULL,
   StartTime time NOT NULL,
-  Subject varchar(50),
+  Subject varchar(10),
 
   ADD CONSTRAINT PK_Lesson_Time PRIMARY KEY (Day, StartTime)
 );
@@ -33,11 +33,23 @@ CREATE TABLE IF NOT EXISTS Lessons (
 CREATE TABLE IF NOT EXISTS LessonStaff (
   Day varchar(10) NOT NULL,
   StartTime time NOT NULL,
-  Name varchar(255) NOT NULL,
+  StaffMember varchar(50) NOT NULL,
 
-  ADD CONSTRAINT PK_Lesson_Staff PRIMARY KEY (Day, StartTime, Staff),
+  ADD CONSTRAINT PK_Lesson_Staff PRIMARY KEY (Day, StartTime, StaffMember),
 
   ADD CONSTRAINT FOREIGN KEY(Day) REFERENCES Lessons(Day),
   ADD CONSTRAINT FOREIGN KEY(StartTime) REFERENCES Lessons(StartTime),
-  ADD CONSTRAINT FOREIGN KEY(Name) REFERENCES Staff(Name)
+  ADD CONSTRAINT FOREIGN KEY(StaffMember) REFERENCES Staff(Name)
+);
+
+CREATE TABLE IF NOT EXISTS LessonAttendees (
+  Day varchar(10) NOT NULL,
+  StartTime time NOT NULL,
+  Student varchar(50) NOT NULL,
+
+  ADD CONSTRAINT PK_Lesson_Students PRIMARY KEY (Day, StartTime, Student),
+
+  ADD CONSTRAINT FOREIGN KEY(Day) REFERENCES Lessons(Day),
+  ADD CONSTRAINT FOREIGN KEY(StartTime) REFERENCES Lessons(StartTime),
+  ADD CONSTRAINT FOREIGN KEY(Student) REFERENCES Students(Name)
 );
